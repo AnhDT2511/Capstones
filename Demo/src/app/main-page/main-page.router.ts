bf4e0a397d8026433af6eab52fcff0a8ad10f08a
hoanglong1312 <hoanglong1312@gmail.com>
2017-12-21 01:09:29 +0700

    Sửa phân quyền profile

--- Demo/src/app/main-page/main-page.router.ts
+++ Demo/src/app/main-page/main-page.router.ts
@@ -14,7 +14,7 @@ const MainPage_Router: Routes = [
             // localhost:4200/main/admin
             { path: 'admin', loadChildren: './admin-page/admin-page.module#AdminPageModule', canActivate : [AuthGuard] },
             // localhost:4200/main/profile 
-            { path: 'profile/:id', loadChildren: './profile-page/profile-page.module#ProfilePageModule' },
+            { path: 'profile/:id', loadChildren: './profile-page/profile-page.module#ProfilePageModule', canActivate : [AuthGuard] },
             // localhost:4200/main/search
             { path: 'search/:id/:text', loadChildren: './search-page/search-page.module#SearchPageModule' },
             // localhost:4200/main/listpost
